9743402509383472b16da45924c3fcdad5852997
Nicholle Romero <Nicholleromero@gmail.com>
2016-04-05 19:01:35 -0700

    new suff2

--- objects.js
+++ objects.js
@@ -15,11 +15,38 @@ function cookieShop (shop, min, max, average) {
   }
 }
 
+function evaluateForm(userForm) {
+if (userForm.store.value == "") {
+  userForm.store.setAttribute("class", "required");
+  alert("Please enter in store name.");
+}
+if (userForm.min.value == ""){
+  userForm.min.setAttribute("class", "required");
+  alert("Please enter in minimum customers.");
+}
+if (userForm.max.value == ""){
+  userForm.max.setAttribute("class", "required");
+  alert("Please enter in maximum customers.");
+}
+if (userForm.average.value == ""){
+  userForm.average.setAttribute("class", "required");
+  alert("Please enter in average cookies purchased.");
+}
+var newName = userForm.store.value;
+var newMin = parseInt(userForm.min.value);
+var newMax = parseInt(userForm.max.value);
+var newAvg = parseInt(userForm.average.value);
+var newStore = new cookieShop (newName, newMin, newMax, newAvg);
+addNumbersToList(newStore)
+}
+
+
 var pioneerSquare = new cookieShop ('Pioneer Square', 17, 88, 5.2);
 var portlandAirport = new cookieShop ('Portland Airport', 6, 24, 1.2);
 var washingtonSquare = new cookieShop ('Washington Square', 11, 38, 1.9);
 var sellwood = new cookieShop ('Sellwood', 20, 48, 3.3);
 var pearlDistrict = new cookieShop ('Pearl District', 3, 24, 2.6);
+
 
 var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 var shops = [pioneerSquare, portlandAirport, washingtonSquare, sellwood, pearlDistrict]
@@ -35,22 +62,23 @@ function addNumbers () {
   listItem.innerHTML = sellwood.numCustomers();
   var listItem = document.getElemntById("pearlDistrict");
   listItem.innerHTML = pearlDistrict.numCustomers();
+  var listItem = document.getElemntById("newStore");
+  listItem.innerHTML = pearlDistrict.numCustomers();
 }
 
 
 
-function addNumbersToList(place, cart) {
-  var list = document.getElementById(place);
-//  for (var x = 0; x < hours.length; x++) {
-//    var listNumber = "<th>"+hours[x]+"</th>"
-//    list.innerHTML += listNumber;
-//  }
-    for (var y = 0; y <hours.length; y++) {
-      var listAmount = "<td>"+cart.numCustomers()+"</td>"
-    //  ": "+cart.numCustomers()+" cookies"+"</li>";
+function addNumbersToList(cart) {
+  var list = document.getElementById("place");
 
-      list.innerHTML += listAmount;
+
+  var shopData = "<tr>"+"<td>"+cart.shop+"</td>"
+  for (var y = 0; y <hours.length; y++) {
+      shopData += "<td>"+cart.numCustomers()+"</td>"
   }
+  shopData += "</tr>"
+  list.innerHTML += shopData
+
 }
 //    list.innerHTML += "<li>"+"Total: "+cart.total+" cookies"+"</li>"
 var listItem = document.getElementById("hours");
@@ -65,9 +93,10 @@ shops.push(portlandAirport);
 shops.push(washingtonSquare);
 shops.push(sellwood);
 shops.push(pearlDistrict);
+//shops.push(newStore);
 
-addNumbersToList("pioneerSquare", pioneerSquare);
-addNumbersToList("portlandAirport", portlandAirport);
-addNumbersToList("washingtonSquare", washingtonSquare);
-addNumbersToList("sellwood", sellwood);
-addNumbersToList("pearlDistrict", pearlDistrict);
+addNumbersToList(pioneerSquare);
+addNumbersToList(portlandAirport);
+addNumbersToList(washingtonSquare);
+addNumbersToList(sellwood);
+addNumbersToList(pearlDistrict);
